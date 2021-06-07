1e98dcba98f80fc0b6facb70133aa96823a5d57e
Dragomir <71335022+DragomirBotezatTI@users.noreply.github.com>
2021-06-07 11:04:48 +0300

    Add files via upload

---
+++ Mediator/Mediator/Mediator/FacebookGroupMediator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Documents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator
+{
+    public interface FacebookGroupMediator
+    {
+        void SendMessage(string msg, User user);
+        void RegisterUser(User user);
+    }
+}
